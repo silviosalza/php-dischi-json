28ec7bd88cadbe1c5964541a28ae0884d3495fdc
silviosalza <silviofilipposalza@gmail.com>
2023-05-08 18:12:09 +0200

    css added

--- index.php
+++ index.php
@@ -7,22 +7,22 @@
     <title>Todo List</title>
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <!-- Font awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Vue js -->
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
     <!-- Axios -->
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <link rel="stylesheet" href="./style.css">
 </head>
-<body>
-<!DOCTYPE html>
-<html lang="en">
+
 <body>
     <div id="app">
-        <div class="container">
+        <div class="wrapper">
+            <div class="header">
+                <img class="logo" src="./img/Spotify_Logo_CMYK_Green.png" alt="">
+            </div>
             <div class="row">
-                <div v-for="(disc,index) in dischi" :key="index" class="col-4">
-                    <div class="card" style="width: 18rem;">
+                <div v-for="(disc,index) in dischi" :key="index" class="col-4 d-flex justify-content-center">
+                    <div class="card my-5" style="width: 18rem;">
                         <img :src="disc.poster"  class="card-img-top" alt="...">
                         <div class="card-body text-center">
                             <p class="card-text">{{disc.title}}</p>
@@ -35,7 +35,5 @@
         </div>
     </div>
     <script src="js/script.js"></script>
-</body>
-</html>
 </body>
 </html>
